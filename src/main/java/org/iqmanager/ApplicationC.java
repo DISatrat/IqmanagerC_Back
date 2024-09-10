a11b539a5da436e1495b925ebda3e3b82f62e36f
DISatrat <dimaneopal@mail.ru>
2024-09-10 11:12:54 +0300

    dev props

--- src/main/java/org/iqmanager/ApplicationC.java
+++ src/main/java/org/iqmanager/ApplicationC.java
@@ -19,8 +19,8 @@ import javax.servlet.MultipartConfigElement;
 @EnableCaching
 public class ApplicationC extends SpringBootServletInitializer {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/iqmanager";
-    //    private static final String URL = "jdbc:mysql://localhost:3306/iqmanager";
+//    private static final String URL = "jdbc:mysql://localhost:3306/iqmanager";
+        private static final String URL = "jdbc:mysql://79.174.88.69:17514/iqmanager";
     private static final String USER = "root123";
 //    private static final String USER = "root";
     private static final String PASSWORD = "rOor!23$";
